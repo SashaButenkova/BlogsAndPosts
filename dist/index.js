6ad06b481185fa0d99f00545231866366898c96e
Sasha Butenkova <butenkova66@gmail.com>
2024-07-29 20:34:26 +0300

    tut

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,10 @@ const express_1 = __importDefault(require("express"));
 const blogs_router_1 = require("./routes/blogs-router");
 exports.app = (0, express_1.default)();
 exports.jsonBodyMiddleware = express_1.default.json();
+//app.use(bodyParser())
 exports.app.use(exports.jsonBodyMiddleware);
 exports.app.use('/blogs', blogs_router_1.blogsRouter);
 const port = process.env.PORT || 5000;
-exports.app.listen(port, () => {
+exports.app.listen(port, async () => {
     console.log(`app listen on porte ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express'
 import { blogsRouter } from './routes/blogs-router'
 import { blogsDb } from './repositories/BlogsRepository'
+import bodyParser from 'body-parser'
 
 export const app = express()
 
 export const jsonBodyMiddleware = express.json()
+
+//app.use(bodyParser())
 
 app.use(jsonBodyMiddleware)
 
@@ -12,6 +15,6 @@ app.use('/blogs', blogsRouter)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
+app.listen(port, async () => {
 	console.log(`app listen on porte ${port}`)
 })
